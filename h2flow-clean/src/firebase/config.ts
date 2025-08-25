48190f7730c20f80ccb220cf649311cfa5429477
mbnel123 <nelissenmb@gmail.com>
2025-08-25 16:09:50 +0200

    Update config.ts

--- h2flow-clean/src/firebase/config.ts
+++ h2flow-clean/src/firebase/config.ts
@@ -1,8 +1,9 @@
+// src/firebase/config.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyD8v4eIC6mtQWh8cn_l3R2Tg7uCXjwsfik",
   authDomain: "h2-flow.firebaseapp.com",
   projectId: "h2-flow",
@@ -12,9 +13,13 @@ const config = {
   measurementId: "G-E61BHHQ29Y"
 };
 
-const app = initializeApp(config);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 
 export default app;
