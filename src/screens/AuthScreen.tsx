2069b3315509173c4eb847aa3b6cf7d0fb506a81
mbnel123 <nelissenmb@gmail.com>
2025-08-25 09:59:44 +0200

    Delete src directory

--- src/screens/AuthScreen.tsx
+++
@@ -1,1 +0,0 @@
-import { View, Text } from "react-native"; export default function AuthScreen() { return <View><Text>Auth</Text></View>; }
--- src/screens/WelcomeScreen.tsx
+++
@@ -1,1 +0,0 @@
-import { View, Text } from "react-native"; export default function WelcomeScreen() { return <View><Text>Welcome</Text></View>; }
